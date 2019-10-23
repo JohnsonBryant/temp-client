2972cdd5dad88647ad0e297e115d77a346bfb3a5
wenzheng yang <1966318133@qq.com>
2019-10-23 22:24:26 +0800

    1. 新增设备页，新增设备接口调通。

--- src/pages/AddEquipment/AddEquipment.vue
+++ src/pages/AddEquipment/AddEquipment.vue
@@ -3,45 +3,62 @@
     <el-row style="margin-bottom:10px;">
       <el-col :span="24">
         <el-card shadow="always">新增设备</el-card>
-        <div class="addform-container">    
-          <!-- 新增测试设备头区块，同一委托单位下可选择批量添加或单个添加 -->
-          <div class="header">
-            <el-input class="company-input" placeholder="请输入委托单位名称" v-model="company">
-              <template slot="prepend">委托单位：</template>
-            </el-input>
-            <el-button class="submit-btn" type="success" icon="el-icon-check" round
-              @click="submitAddEquipment"
-            >提交</el-button>
+          <div class="addform-container">
+            <!-- 新增测试设备头区块，同一委托单位下可选择批量添加或单个添加 -->
+            <div class="header">
+                <el-input class="company-input" placeholder="请输入委托单位名称" v-model="company">
+                  <template slot="prepend">委托单位：</template>
+                </el-input>
+              <el-button class="submit-btn" type="success" icon="el-icon-check" round
+                @click="submitAddEquipment"
+              >提交</el-button>
+            </div>
+            <el-divider content-position="center">设备信息</el-divider>
+            <!-- 新增设备主体区块 -->
+            <div class="body">
+              <el-row :gutter="15">
+                <!-- 单个测试设备信息输入模块 -->
+                <equipment-info-input 
+                  v-for="(info,index) in equipmentInfo" :key="index"
+                  :info="info"
+                  :rulesInfo="rulesInfo"
+                />
+                <!-- 新增单个测试设备信息输入模块 -->
+                <el-col :span="8">
+                  <el-card class="box-card box-item box-add-btn">
+                    <el-button class="btn btn-add" type="success" icon="el-icon-plus" circle
+                      @click="addEqInputItem"
+                    ></el-button>
+                    <el-button class="btn btn-delete" type="danger" icon="el-icon-minus" circle
+                      @click="deleteEqInputItem"
+                    ></el-button>
+                  </el-card>
+                </el-col>
+              </el-row>
+            </div>
           </div>
-          <el-divider content-position="center">设备信息</el-divider>
-          <div class="body">
-            <el-row :gutter="15">
-              <!-- 单个测试设备信息输入模块 -->
-              <equipment-info-input 
-                v-for="(info,index) in equipmentInfo" :key="index"
-                :info="info"
-              />
-              <!-- 新增单个测试设备信息输入模块 -->
-              <el-col :span="8">
-                <el-card class="box-card box-item box-add-btn">
-                  <el-button class="btn btn-add" type="success" icon="el-icon-plus" circle
-                    @click="addEqInputItem"
-                  ></el-button>
-                  <el-button class="btn btn-delete" type="danger" icon="el-icon-minus" circle
-                    @click="deleteEqInputItem"
-                  ></el-button>
-                </el-card>
-              </el-col>
-            </el-row>
-          </div>
-        </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import EquipmentInfoInput from './EquipmentInfoInput'
+import EquipmentInfoInput from './EquipmentInfoInput';
+
+let rulesInfo = {
+  em: [
+    { required: true, message: '请输入设备厂家', trigger: ['blur', 'change']}
+  ],
+  deviceName: [
+    { required: true, message: '请输入设备名称', trigger: ['blur', 'change']}
+  ],
+  deviceType: [
+    { required: true, message: '请输入设备类型', trigger: ['blur', 'change']}
+  ],
+  deviceID: [
+    { required: true, message: '请输入设备ID', trigger: ['blur', 'change']}
+  ],
+};
 
 export default {
   name: 'add-equipment',
@@ -53,7 +70,8 @@ export default {
       company: '',
       equipmentInfo: [
         {em: '', deviceName: '', deviceType: '', deviceID: ''},
-      ]
+      ],
+      rulesInfo: rulesInfo
     }
   },
   methods: {
@@ -64,24 +82,44 @@ export default {
       this.equipmentInfo.pop({em: '', deviceName: '', deviceType: '', deviceID: ''})
     },
     submitAddEquipment() {
-      // 打包已输入的设备信息数组
-      let equipmentInfo = this.equipmentInfo.slice().map((item) => {
-        item.company = this.company
-        return item
-      })
-      console.log(equipmentInfo)
-      //POST至后端新增测试设备接口
-      this.axios.post(this.util.testApi() + '/addEquipment', equipmentInfo)
-        .then(res => {
-          res.data.forEach((item) => {
-            item.value = item.company
+      // 输入的设备信息检查 : 输入不能为空
+      this.$refs['info'].validate((valid) => {
+        if (valid) {
+          // 输入的设备不能存在完全相同
+          
+          let dnow = this.util.nowtime()
+          // 打包已输入的设备信息数组
+          let equipmentInfo = this.equipmentInfo.slice().map((item) => {
+            item.company = this.company
+            item.insertDate = dnow
+            return item
           })
-          this.companys = res.data
-        })
-        .catch(err => {
-          console.log(err)
-        })
-    }
+          console.log(equipmentInfo)
+          //POST至后端新增测试设备接口
+          this.axios.post(this.util.testApi() + '/addEquipment', equipmentInfo)
+            .then(res => {
+              if (res.data.isSuccessed) {
+                this.addMessage('设备新增成功！', 'success');
+              } else {
+                this.addMessage('设备新增失败！', 'warning');
+              }
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        }
+      });
+    },
+    validateEquipmentInfo() {
+      let ret = true
+      // 检查
+    },
+    addMessage(message, messageType) {
+      this.$message({
+        message: message,
+        type: messageType
+      });
+    },
   }
 };
 </script>
--- src/pages/AddEquipment/EquipmentInfoInput.vue
+++ src/pages/AddEquipment/EquipmentInfoInput.vue
@@ -1,26 +1,42 @@
 <template>
 <el-col :span="8">
   <el-card class="box-card box-item">
-    <el-input class="device-input" placeholder="请输入设备厂家" v-model="info.em">
-      <template slot="prepend">设备厂家：</template>
-    </el-input>
-    <el-input class="device-input" placeholder="请输入设备型号" v-model="info.deviceName">
-      <template slot="prepend">设备名称：</template>
-    </el-input>
-    <el-input class="device-input" placeholder="请输入设备型号" v-model="info.deviceType">
-      <template slot="prepend">设备型号：</template>
-    </el-input>
-    <el-input class="device-input" placeholder="请输入设备编号" v-model="info.deviceID">
-      <template slot="prepend">设备编号：</template>
-    </el-input>
+      <el-form :model="info" :rules="rulesInfo" ref="info" >
+        <el-form-item prop="em">
+          <el-input class="device-input" placeholder="请输入设备厂家" v-model="info.em">
+            <template slot="prepend">设备厂家：</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="deviceName">
+          <el-input class="device-input" placeholder="请输入设备型号" v-model="info.deviceName">
+            <template slot="prepend">设备名称：</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="deviceType">
+          <el-input class="device-input" placeholder="请输入设备型号" v-model="info.deviceType">
+            <template slot="prepend">设备型号：</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="deviceID">
+          <el-input class="device-input" placeholder="请输入设备编号" v-model="info.deviceID">
+            <template slot="prepend">设备编号：</template>
+          </el-input>
+        </el-form-item>
+      </el-form>
   </el-card>
 </el-col>
 </template>
 
 <script>
+
 export default {
   name: 'EquipmentInfoInput',
-  props: ["info"]
+  props: ['info', 'rulesInfo'],
+  data() {
+    return {
+      
+    }
+  }
 }
 </script>
 
@@ -28,13 +44,13 @@ export default {
 
 .box-item{
   position: relative;
-  padding-top: 30px;
+  padding-top: 15px;
   height: 360px;
   margin-bottom: 15px;
 }
 
 .device-input {
-  margin-bottom: 30px;
+  margin-bottom: 15px;
 }
 
 .box-add-btn .btn {
